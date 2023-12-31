5d9f85b8c1e5168bebd38d6240757894244c9bb2
jookie <dovpeles@gmail.com>
2023-12-31 18:54:24 -0500

    Initial commit

--- src/components/componentsCap/HeroSection.tsx
+++ src/components/componentsCap/HeroSection.tsx
@@ -13,56 +13,56 @@ export default function HeroSection() {
     }, 2000);
   };
   return (
-    <main className="flex w-full flex-col items-center justify-center text-center px-4 sm:mt-2 mt-20">
-      <div className="flex justify-center space-x-4">
-        <img
-          alt="Israeli Soldier on and a building"
-          src="src/img/banner/factChecking.png"
-          className="w-96 h-96 rounded-2xl"
-        />
+    <main
+    >
+      <div
+        className="flex flex-col items-center justify-center text-center sm:mt-2 mt-20 flex-1 px-4 w-full"
+      >
+        {/* banner start*/}
+        <div className="flex flex-wrap">
+          <div className="w-2/3 md:w-1/4 p-2">
+            <img
+              alt="banner_left"
+              src="src/img/banner/factChecking.png"
+              className ="w-full h-auto"
+            />
+          </div>
+          <div className="w-1/3 md:w-1/4 p-2">
+            <img
+              alt="banner_right"
+              src="src/img/banner/azaMarchSouth.png"
+              className ="w-full h-auto"
+            />
+          </div>
+        </div>
+        {/* banner end*/}
+        <div className="">
+          <textarea className="w-full h-full p-2 text-gray-700 border border-gray-300 resize-none focus:outline-none focus:ring"
+            placeholder="Enter text here..."
+            value="A soldier standing on top of a building in Sderot - Israel"
+          />
+          <button
+            className="px-400 py-2 text-white bg-[#5a5cd1] rounded-md hover:bg-[#3f4194] focus:outline-none focus:ring flex w-1/2 p-4"
+            onClick={() =>
+              copyToClipboard(
+                "A soldier standing on top of a building in Sderot - Israel"
+              )}
+          >
+            <div>
+              <HiOutlineClipboardDocument className="w-5 h-5 mr-1" />
+              {buttonText}
+            </div>
+          </button>
+        </div>
         <div className="sm:mt-0 mt-8 w-full md:w-1/2">
           <button className="px-4 py-2 text-white bg-[#5a5cd1] rounded-md hover:bg-[#3f4194] focus:outline-none focus:ring"
             onClick={() => {
               window.location.href = "https://captionai-seven.vercel.app/";
-            }
-            } >
+            }} >
             Scan Social Media on line in real time
           </button>
-        </div>
-      </div>
-      <div >
-        <div >
-          <div >
-            <div className="w-full md:w-1/2">
-              <img
-                alt="Israeli Soldier on and a building"
-                src="src/img/banner/azaMarchSouth.png"
-                className="w-96 h-96 rounded-2xl"
-                width={500}
-                height={200}
-              />
-            </div>
-            <div >
-              <textarea className="w-full h-full p-2 text-gray-700 border border-gray-300 rounded-md resize-none focus:outline-none focus:ring"
-                placeholder="Enter text here..."
-                value="a soldier standing on top of a building in Sderot - Israel"
-              />
-              <button className="px-400 py-2 text-white bg-[#5a5cd1] rounded-md hover:bg-[#3f4194] focus:outline-none focus:ring"
-                onClick={() =>
-                  copyToClipboard(
-                    "a soldier standing on top of a building in Sderot - Israel"
-                  )
-                }
-              >
-                <div>
-                  <HiOutlineClipboardDocument className="w-5 h-5 mr-1" />
-                  {buttonText}
-                </div>
-              </button>
-            </div>
-          </div>
-        </div>
-      </div>
+        </div> {/* scan button*/}
+      </div>{/* main */}
     </main>
   );
 }
